dc550a019f50f4602d71b282d1010a7d67a72c70
Madison Beard <527mbeard@frhsd.com>
2026-05-21 03:53:20 +0000

    correct changes up to incoming changes commit

--- collection.js
+++ collection.js
@@ -14,7 +14,6 @@ document.getElementById("darkModeBtn").addEventListener("click", function () {
 });
 // dark mode
 
-
 const collection = [
     {
         title: "The Gray Man",
@@ -25,8 +24,9 @@ const collection = [
         streaming_platforms: ["Netflix"],
         actors: ["Ryan Gosling", "Chris Evans", "Ana de Armas"],
         runtime: "2h 9m",
-        description: "A skilled CIA operative uncovers dangerous government secrets and becomes the target of a relentless international manhunt led by a ruthless assassin.",
-        image: "grayman.jpg"
+        description:
+            "A skilled CIA operative uncovers dangerous government secrets and becomes the target of a relentless international manhunt led by a ruthless assassin.",
+        image: "grayman.jpg",
     },
     {
         title: "Fast and Furious Presents: Hobbs and Shaw",
@@ -37,8 +37,9 @@ const collection = [
         streaming_platforms: ["Netflix"],
         actors: ["Dwayne Johnson", "Jason Statham", "Vanessa Kirby"],
         runtime: "2h 17m",
-        description: "Former enemies Luke Hobbs and Deckard Shaw are forced to team up when a cyber-enhanced villain threatens the world with a deadly virus.",
-        image: "hobbshaw.jpg"
+        description:
+            "Former enemies Luke Hobbs and Deckard Shaw are forced to team up when a cyber-enhanced villain threatens the world with a deadly virus.",
+        image: "hobbshaw.jpg",
     },
     {
         title: "The Beekeeper",
@@ -49,8 +50,9 @@ const collection = [
         streaming_platforms: ["Hulu"],
         actors: ["Jason Statham", "Josh Hutcherson", "Jeremy Irons"],
         runtime: "1h 45m",
-        description: "After a close friend falls victim to a massive scam operation, a quiet beekeeper with a mysterious past begins a brutal quest for revenge. As he takes down powerful criminals, secrets about his former life start to surface.",
-        image: "bee.jpg"
+        description:
+            "After a close friend falls victim to a massive scam operation, a quiet beekeeper with a mysterious past begins a brutal quest for revenge. As he takes down powerful criminals, secrets about his former life start to surface.",
+        image: "bee.jpg",
     },
     {
         title: "Heads of State",
@@ -61,8 +63,9 @@ const collection = [
         streaming_platforms: ["Prime Video"],
         actors: ["Idris Elba", "John Cena", "Priyanka Chopra Jonas"],
         runtime: "1h 53m",
-        description: "When world leaders become the targets of a dangerous global threat, two completely different political figures must work together to survive.",
-        image: "state.jpg"
+        description:
+            "When world leaders become the targets of a dangerous global threat, two completely different political figures must work together to survive.",
+        image: "state.jpg",
     },
     {
         title: "Zootopia 2",
@@ -73,8 +76,9 @@ const collection = [
         streaming_platforms: ["Disney+"],
         actors: ["Ginnifer Goodwin", "Jason Bateman", "Andy Samberg"],
         runtime: "1h 48m",
-        description: "Judy Hopps and Nick Wilde return for another adventure in the busy animal city of Zootopia. As they investigate a new mystery, the pair face unexpected challenges that test their friendship and detective skills.",
-        image: "zoo.jpg"
+        description:
+            "Judy Hopps and Nick Wilde return for another adventure in the busy animal city of Zootopia. As they investigate a new mystery, the pair face unexpected challenges that test their friendship and detective skills.",
+        image: "zoo.jpg",
     },
     {
         title: "Puss In Boots Last Wish",
@@ -85,8 +89,9 @@ const collection = [
         streaming_platforms: ["Peacock"],
         actors: ["Antonio Banderas", "Salma Hayek", "Florence Pugh"],
         runtime: "1h 42m",
-        description: "After discovering he has only one life left, the fearless Puss in Boots sets out on a journey to find a magical wishing star that could restore his lost lives. Along the way, he faces dangerous enemies while learning the value of friendship and courage.",
-        image: "wish.jpg"
+        description:
+            "After discovering he has only one life left, the fearless Puss in Boots sets out on a journey to find a magical wishing star that could restore his lost lives. Along the way, he faces dangerous enemies while learning the value of friendship and courage.",
+        image: "wish.jpg",
     },
     {
         title: "Cars 2",
@@ -97,8 +102,9 @@ const collection = [
         streaming_platforms: ["Disney+"],
         actors: ["Owen Wilson", "Emily Mortimer", "Micheal Caine"],
         runtime: "1h 46m",
-        description: "Lightning McQueen heads overseas to compete in an international racing competition, but Mater accidentally becomes involved in a secret spy mission.",
-        image: "cars2.jpg"
+        description:
+            "Lightning McQueen heads overseas to compete in an international racing competition, but Mater accidentally becomes involved in a secret spy mission.",
+        image: "cars2.jpg",
     },
     {
         title: "Bad Guys 2",
@@ -109,8 +115,9 @@ const collection = [
         streaming_platforms: ["Netflix"],
         actors: ["Sam Rockwell", "Awkwafina", "Anthony Ramos"],
         runtime: "1h 44m",
-        description: "The Bad Guys return for another hilarious adventure as the former criminals try to continue their new lives as heroes. Their latest mission quickly spirals into chaos when they encounter a fresh group of dangerous rivals.",
-        image: "badguys.jpg"
+        description:
+            "The Bad Guys return for another hilarious adventure as the former criminals try to continue their new lives as heroes. Their latest mission quickly spirals into chaos when they encounter a fresh group of dangerous rivals.",
+        image: "badguys.jpg",
     },
     {
         title: "People We Meet On Vacation",
@@ -121,8 +128,9 @@ const collection = [
         streaming_platforms: ["Netflix"],
         actors: ["Tom Bylth", "Emily Bader", "Miles Heizer"],
         runtime: "1h 58m",
-        description: "Two best friends with opposite personalities reunite for one last vacation after years of shared summer trips. As old memories resurface, they begin to realize their relationship may have always been more than friendship.",
-        image: "vacation.jpg"
+        description:
+            "Two best friends with opposite personalities reunite for one last vacation after years of shared summer trips. As old memories resurface, they begin to realize their relationship may have always been more than friendship.",
+        image: "vacation.jpg",
     },
     {
         title: "Purple Hearts",
@@ -133,8 +141,9 @@ const collection = [
         streaming_platforms: ["Netflix"],
         actors: ["Nicholas Galitzine", "Sofia Carson", "Nicholas Duvernay"],
         runtime: "2h 2m",
-        description: "An aspiring musician and a struggling Marine agree to a fake marriage for financial benefits, but their arrangement slowly develops into a real emotional connection.",
-        image: "purpleheart.jpg"
+        description:
+            "An aspiring musician and a struggling Marine agree to a fake marriage for financial benefits, but their arrangement slowly develops into a real emotional connection.",
+        image: "purpleheart.jpg",
     },
     {
         title: "My Oxford Year",
@@ -145,8 +154,9 @@ const collection = [
         streaming_platforms: ["Netflix"],
         actors: ["Sofia Carson", "Corey Mylchreest", "Dougray Scott"],
         runtime: "1h 52m",
-        description: "An ambitious American student moves to Oxford University to pursue her dream program but unexpectedly falls in love with a charming local man, changing the future she planned for herself.",
-        image: "oxford.jpg"
+        description:
+            "An ambitious American student moves to Oxford University to pursue her dream program but unexpectedly falls in love with a charming local man, changing the future she planned for herself.",
+        image: "oxford.jpg",
     },
     {
         title: "My Fault: London",
@@ -157,8 +167,9 @@ const collection = [
         streaming_platforms: ["Prime Video"],
         actors: ["Asha Banks", "Matthew Broome", "Eve Macklin"],
         runtime: "1h 59m",
-        description: "In a futuristic society obsessed with entertainment, a desperate man enters a deadly televised competition where contestants are hunted for the public’s enjoyment. As he fights to survive, he becomes a symbol of rebellion against the system.",
-        image: "london.jpg"
+        description:
+            "In a futuristic society obsessed with entertainment, a desperate man enters a deadly televised competition where contestants are hunted for the public’s enjoyment. As he fights to survive, he becomes a symbol of rebellion against the system.",
+        image: "london.jpg",
     },
     {
         title: "The Running Man",
@@ -169,8 +180,9 @@ const collection = [
         streaming_platforms: ["Paramount+"],
         actors: ["Glen Powell", "Josh Brolin", "Colman Domingo"],
         runtime: "2h 9m",
-        description: "After moving to London with her mother, a rebellious teenager struggles to adjust to her new wealthy lifestyle while forming a complicated connection with her stepbrother. Their relationship grows increasingly intense as family tensions rise.",
-        image: "running.jpg"
+        description:
+            "After moving to London with her mother, a rebellious teenager struggles to adjust to her new wealthy lifestyle while forming a complicated connection with her stepbrother. Their relationship grows increasingly intense as family tensions rise.",
+        image: "running.jpg",
     },
     {
         title: "Edge of Tomorrow",
@@ -181,8 +193,9 @@ const collection = [
         streaming_platforms: ["HBO Max"],
         actors: ["Tom Cruise", "Emily Blunt", "Bill Paxton"],
         runtime: "1h 53m",
-        description: "A military officer with little combat experience becomes trapped in a time loop during an alien invasion, reliving the same deadly battle repeatedly while trying to save humanity.",
-        image: "tomorrow.jpg"
+        description:
+            "A military officer with little combat experience becomes trapped in a time loop during an alien invasion, reliving the same deadly battle repeatedly while trying to save humanity.",
+        image: "tomorrow.jpg",
     },
     {
         title: "The Hunger Games",
@@ -193,8 +206,9 @@ const collection = [
         streaming_platforms: ["Peacock"],
         actors: ["Jennifer Lawrence", "Josh Hutcherson", "Liam Hemsworth"],
         runtime: "2h 22m",
-        description: "Katniss Everdeen volunteers to take her sister’s place in the Hunger Games, a brutal televised competition where teenagers fight for survival in a dystopian society.",
-        image: "games.jpg"
+        description:
+            "Katniss Everdeen volunteers to take her sister’s place in the Hunger Games, a brutal televised competition where teenagers fight for survival in a dystopian society.",
+        image: "games.jpg",
     },
     {
         title: "Alita: Battle Angel",
@@ -205,8 +219,9 @@ const collection = [
         streaming_platforms: ["Hulu"],
         actors: ["Rosa Salazar", "Christoph Waltz", "Keean Johnson"],
         runtime: "2h 2m",
-        description: "A cyborg with no memory of her past is rebuilt by a scientist and discovers extraordinary fighting abilities while uncovering secrets about her identity and the futuristic world around her.",
-        image: "angel.png"
+        description:
+            "A cyborg with no memory of her past is rebuilt by a scientist and discovers extraordinary fighting abilities while uncovering secrets about her identity and the futuristic world around her.",
+        image: "angel.png",
     },
     {
         title: "Captain America: The Winter Soldier",
@@ -217,8 +232,9 @@ const collection = [
         streaming_platforms: ["Disney+"],
         actors: ["Chris Evans", "Scarlett Johansson", "Sebastian Stan"],
         runtime: "2h 16m",
-        description: "Steve Rogers uncovers a massive conspiracy within S.H.I.E.L.D. while facing a mysterious assassin known as the Winter Soldier, forcing him to question who he can trust.",
-        image: "soilder.jpg"
+        description:
+            "Steve Rogers uncovers a massive conspiracy within S.H.I.E.L.D. while facing a mysterious assassin known as the Winter Soldier, forcing him to question who he can trust.",
+        image: "soilder.jpg",
     },
     {
         title: "Black Panther 1",
@@ -229,8 +245,9 @@ const collection = [
         streaming_platforms: ["Netflix"],
         actors: ["Chadwick Boseman", "Michael B. Jordan", "Lupita Nyong'o"],
         runtime: "2h 14m",
-        description: "After becoming king of Wakanda, T’Challa must defend his nation and the Black Panther mantle from a challenger connected to his family’s past.",
-        image: "panther.jpg"
+        description:
+            "After becoming king of Wakanda, T’Challa must defend his nation and the Black Panther mantle from a challenger connected to his family’s past.",
+        image: "panther.jpg",
     },
     {
         title: "Black Widow",
@@ -241,8 +258,9 @@ const collection = [
         streaming_platforms: ["Disney+"],
         actors: ["Scarlett Johansson", "Florence Pugh", "David Harbour"],
         runtime: "2h 14m",
-        description: "Natasha Romanoff reunites with figures from her past while confronting the dangerous organization that trained her to become the Black Widow.",
-        image: "widow.jpg"
+        description:
+            "Natasha Romanoff reunites with figures from her past while confronting the dangerous organization that trained her to become the Black Widow.",
+        image: "widow.jpg",
     },
     {
         title: "Thunderbolts",
@@ -253,12 +271,13 @@ const collection = [
         streaming_platforms: ["Disney+"],
         actors: ["Florence Pugh", "Sebastian Stan", "Lewis Pullman"],
         runtime: "2h 6m",
-        description: "A group of antiheroes and former villains are brought together for dangerous government missions, forcing them to work as a team despite their complicated pasts.",
-        image: "thunderbolts.jpeg"
+        description:
+            "A group of antiheroes and former villains are brought together for dangerous government missions, forcing them to work as a team despite their complicated pasts.",
+        image: "thunderbolts.jpeg",
     },
-]
+];
 
-console.log(collection)
+console.log(collection);
 
 const movieGrid = document.getElementById("movieGrid");
 
@@ -273,9 +292,7 @@ let currentFilter = "All";
 function render(list) {
     movieGrid.innerHTML = "";
 
-    list.forEach(movie => {
-
-        // ✅ CONDITIONAL (RUBRIC REQUIREMENT)
+    list.forEach((movie) => {
         let badgeColor = "";
         if (movie.rating >= 4.5) {
             badgeColor = "gold";
@@ -287,9 +304,10 @@ function render(list) {
         col.className = "col-12 col-sm-6 col-lg-3 d-flex justify-content-center";
 
         const card = document.createElement("div");
-        card.classList.add("movie-card");
+        // card.classList.add("movie-card");
 
         card.innerHTML = `
+                    <div class="movie-card">
             <div class="poster-wrapper">
                 <img src="${movie.image}" class="movie-poster">
             </div>
@@ -309,18 +327,17 @@ function render(list) {
 
                 <div class="bottom-row">
                     <span class="movie-year">${movie.year}</span>
-                    <button class="view-btn">More Info</button>
+                    <button class="view-btn">View Details</button>
+                </div>
                 </div>
 
             </div>
         `;
 
-        // ✅ MODAL (RUBRIC REQUIREMENT)
         card.querySelector(".view-btn").addEventListener("click", () => {
             modalTitle.textContent = movie.title;
             modalDesc.textContent = movie.description;
-            modalExtra.textContent =
-                `Director: ${movie.director} | Runtime: ${movie.runtime}`;
+            modalExtra.textContent = `Director: ${movie.director} | Runtime: ${movie.runtime}`;
 
             modal.classList.remove("hidden");
         });
@@ -330,27 +347,48 @@ function render(list) {
     });
 }
 
-// FILTER FUNCTION (.filter() RUBRIC REQUIREMENT)
 function filterMovies(category) {
     if (category === "All") {
         render(collection);
     } else {
-        const filtered = collection.filter(movie => movie.genre === category);
+        const filtered = collection.filter((movie) => movie.genre === category);
         render(filtered);
     }
 }
 
-// BUTTON EVENTS
-document.querySelectorAll(".filter-btn").forEach(btn => {
+document.querySelectorAll(".sort-option").forEach(option => {
+    option.addEventListener("click", () => {
+
+        const sortType = option.dataset.sort;
+
+        let sortedMovies = [...collection];
+
+        if (sortType === "rating") {
+            sortedMovies.sort((a, b) => b.rating - a.rating);
+        }
+
+        else if (sortType === "alphabetical") {
+            sortedMovies.sort((a, b) =>
+                a.title.localeCompare(b.title)
+            );
+        }
+
+        else if (sortType === "random") {
+            sortedMovies.sort(() => Math.random() - 0.5);
+        }
+
+        render(sortedMovies);
+    });
+});
+
+document.querySelectorAll(".genre-btn").forEach((btn) => {
     btn.addEventListener("click", () => {
         filterMovies(btn.dataset.filter);
     });
 });
 
-// CLOSE MODAL
 closeModal.addEventListener("click", () => {
     modal.classList.add("hidden");
 });
 
-// INITIAL LOAD
 render(collection);
